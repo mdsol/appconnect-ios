fc43c78c43ac3d59508913b77552290cb856fd16
Nolan Carroll <ncarroll@mdsol.com>
2015-12-30 09:49:17 -0500

    MCC-188776 - add text in review controller

--- AppConnectSwift/ReviewViewController.swift
+++ AppConnectSwift/ReviewViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ReviewViewController: UIViewController {
 
+    @IBOutlet var field1Label : UILabel!
+    @IBOutlet var field1Answer : UILabel!
+    @IBOutlet var field2Label : UILabel!
+    @IBOutlet var field2Answer : UILabel!
+    @IBOutlet var field3Label : UILabel!
+    @IBOutlet var field3Answer : UILabel!
+    
     var form: MDForm!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let fields = form.fields
+        
+        let dictionaryField = fields[0] as! MDDictionaryField
+        let dateField = fields[1] as! MDDateTimeField
+        let scaleField = fields[2] as! MDScaleField
+    
+        field1Label.text = dictionaryField.label
+        field1Answer.text = dictionaryField.subjectResponse.userValue
+        
+        field2Label.text = dateField.label
+        field2Answer.text = MDRaveDateFormatter(string: dateField.dateTimeFormat).stringFromDate(dateField.subjectResponse)
+        
+        field3Label.text = scaleField.label
+        field3Answer.text = scaleField.subjectResponse.stringValue
     }
 
 }
